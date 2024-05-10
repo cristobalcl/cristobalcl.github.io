a342a4087a5c0ea55b4a0125baa19288b62c07d8
Cristóbal Carnero Liñán <ccarnerolinan@gmail.com>
2024-05-10 18:42:46 +0200

    Fix paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://cristobal.dev",
   integrations: [mdx(), sitemap(), tailwind()],
+  output: 'static'
 });
